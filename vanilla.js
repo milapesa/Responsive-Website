c125c6f79197759407cf166dcf2907fca6d9482f
Mila Pesa Mazzola <milapesamazzola@gmail.com>
2019-03-23 11:55:06 -0300

    Add files via upload

---
+++ vanilla.js
@@ -0,0 +1,69 @@
+
+window.addEventListener('load', () => {
+  
+   let long;
+   let lat;  
+   let weatherDescription= document.querySelector('.weather-description');
+   let celciusSection= document.querySelector('.celcius-section');
+   let celciusDegree= document.querySelector('.celcius-degree');
+   let locationTimezone= document.querySelector('.location-timezone');
+   let temperatureSection= document.querySelector('.temperature');
+   const temperatureSpan= document.querySelector('.temperature span');
+   
+
+  if (navigator.geolocation) {
+   
+    navigator.geolocation.getCurrentPosition(position => {
+     long = position.coords.longitude;
+  	 lat  = position.coords.latitude;
+
+    const proxy ='https://cors-anywhere.herokuapp.com/';
+    const api =`${proxy}https://api.darksky.net/forecast/95a2d410903b0445b0631c4109b6b959/${lat},${long}`;
+    
+    fetch(api)
+    .then(response => {
+  	return response.json();
+    })
+    .then(data => {
+     	console.log(data)
+      const {temperature,summary,icon,daily } = data.currently;
+      // Set DOM elements from the API!
+      celciusDegree.textContent= temperature;
+      weatherDescription.textContent= summary;
+      locationTimezone.textContent= data.timezone;
+     
+
+      //Formula for Celcius
+      let celcius =(temperature-32) * (5 / 9);
+  
+       //SET ICON
+       setIcons(icon, document.querySelector('.icon'));
+
+
+      //Change temperature to Celcius/Farenheit
+       temperatureSection.addEventListener("click", () => {
+         if(temperatureSpan.textContent === "F°") {
+           temperatureSpan.textContent ="C°";
+           celciusDegree.textContent = Math.floor(celcius);
+         } else {
+           temperatureSpan.textContent = "F°";
+           celciusDegree.textContent = temperature;
+         }
+       });
+      if(locationTimezone.textContent === "America/Argentina/Buenos_Aires"){
+      locationTimezone.textContent = "Buenos Aires, Argentina";
+    } else {locationTimezone.textContent = data.timezone;}
+
+     });
+  });
+ }
+  
+function setIcons(icon, iconID) {  
+  const skycons = new Skycons({color:"grey"});
+  const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+  skycons.play();
+  return skycons.set(iconID, Skycons[currentIcon]);
+
+}
+});
+
